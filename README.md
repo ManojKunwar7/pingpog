e6fe1822069fa222e2cc51aa9d4db291e2e13cbf
manoj.kunwar@repushti.com <manoj.kunwar@repushti.com>
2024-10-22 04:12:39 +0530

    added ball

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ make pingpog // Recompile the game
 ## Useful links
 - https://www.cs.cmu.edu/~ralf/files.html
 - https://www.ctyme.com/intr/int.htm
+- https://en.wikipedia.org/wiki/Mode_13h
 
 [nasm]: https://www.nasm.us/
 [qemu]: https://www.qemu.org/
--- pingpog.asm
+++ pingpog.asm
@@ -1,30 +1,101 @@
-	;; h = high register maybe ? 
+	;; Tells the bios where our boot sector seats in bios 
+	org 0x7C00
+	
+	;; eax = register (16:16)(32 bit)
+	;; x = register h:l (8:8) (16 bit)
+	;; h = register(higher 8 bit)(zero based maybe ?) , l = lower regsiter (8 bit)
+	;; how to define a varibale (macro) %define (variable_name) (variable_value) 
+	%define ROW 50	
+	%define WIDTH 320
+	%define HEIGHT 200 
+
+	%define BALL_WIDTH 10
+	%define BALL_HEIGHT 10
+	
+	;; Enter video mode
 	mov ah, 0x00
 	; Enter VGA Mode 0x13
 	; 320x200 256 colors
 	mov al, 0x13
-	;; Print it to screen maybe
+	;; Print it to screen maybe?
 	int 0x10
 
 	;; (ax is 16 bit value) we will assign 0xA000 this to ax(16 bit value) 
-	;; 0xA000 => holds the content display of video mode "0x13" 
+	;; 0xA000 => holds the content display of video mode "0x13" i.e colors(rgb) 
 	mov ax, 0xA000
 	mov es, ax
-
-	mov bx, 0
-loop:
-	;; byte [0xA000: 0(16 bit)], color
-	mov BYTE [es:bx], 0x0B
-	;; Increment bx + 1;
-	inc bx
-	;; cmp bx == 64000
-	cmp bx, 64000
-	;; jb is jump unsigned integers which is (65,000~)
-	;; & jl is jump signed integers which is (-32,000~ - 32,000~)
-	;; bx is of 16 bit
-	jb loop
+	;; This is a formula maybe 
 	
+	
+;; loop:
+;; 	;; byte [0xA000: 0(16 bit)], color
+;; 	mov BYTE [es:bx], 0x0B
+;; 	;; Increment bx + 1;
+;; 	inc bx
+;; 	;; cmp bx == 64000 
+;; 	;; cmp bx, 64000
+;; 	cmp bx , WIDTH * ROW + WIDTH
+;; 	;; jb is jump unsigned integers which is (65,000~)
+;; 	;; & jl is jump signed integers which is (-32,000~ - 32,000~)
+;; 	;; bx is of 16 bit
+	;; 	jb loop
+
+	mov word [ball_x], 0
+	mov word [ball_y], 0						
+	mov word [ball_dx], 1
+	mov word [ball_dy], 1
+	
+main_loop:
+	mov ch, 0x00
+	call draw_ball
+	
+	mov ax , [ball_x]
+	add ax , [ball_dx]
+	mov [ball_x] , ax
+
+	mov ax , [ball_y]
+	add ax , [ball_dy]
+	mov [ball_y] , ax
+
+	mov ch , 0x0A
+	call draw_ball
+	jmp main_loop 
+
+draw_ball:
 		
-	jmp $
+	mov word [i], 0
+draw_ball_i:										;row
+	mov word [j], 0								;
+draw_ball_j:										;col
+
+	mov ax, WIDTH
+	mov bx, [i]
+	add bx, [ball_y]
+	mul bx
+	mov bx, ax
+	add bx, [j]
+	add bx, [ball_x]
+	mov BYTE [es:bx], ch	
+
+	inc word [j]
+  cmp word [j], BALL_WIDTH
+	jb draw_ball_j
+	
+	inc word [i]
+  cmp word [i], BALL_HEIGHT
+	jb draw_ball_i	
+	ret
+
+;; 1. dd means define double word which allocates 32 bit,
+;; 2. dw means define word which allocates 16 bit ,
+;; 3. db means define byte which allocates 8 bit,
+	
+i: dw 0 
+j: dw 0
+ball_x: dw 0
+ball_y:	dw 0
+ball_dx: dw 0
+ball_dy: dw 0
+	
 	times 510 - ($-$$) db 0
 	dw 0xaa55
